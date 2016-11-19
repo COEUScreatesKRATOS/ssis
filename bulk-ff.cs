71f11be227493eb7bd2f9cb6518f04aad06c2e18
COEUScreatesKRATOS <chardingbrown@gmail.com>
2016-11-18 22:57:52 -0700

    Create table for source files automatically

--- bulk-ff.cs
+++ bulk-ff.cs
@@ -27,8 +27,8 @@ namespace ST_9fe3f0c506744f589075f4e22c70b50b
     /// or parent of this class.
     /// </summary>
 	[Microsoft.SqlServer.Dts.Tasks.ScriptTask.SSISScriptTaskEntryPointAttribute]
-	public partial class ScriptMain : Microsoft.SqlServer.Dts.Tasks.ScriptTask.VSTARTScriptObjectModelBase
-	{
+    public partial class ScriptMain : Microsoft.SqlServer.Dts.Tasks.ScriptTask.VSTARTScriptObjectModelBase
+    {
         #region Help:  Using Integration Services variables and parameters in a script
         /* To use a variable in this script, first ensure that the variable has been added to 
          * either the list contained in the ReadOnlyVariables property or the list contained in 
@@ -88,7 +88,40 @@ namespace ST_9fe3f0c506744f589075f4e22c70b50b
          * */
         #endregion
 
+        //Do all this for Bulk load Multi FF
+        //Add createTable to Bulk Load Multi FF
+        //Add Folder Search and Iter through Folders to get CSV Files (e.g. Each Folder inside "Source Folder" is a Table that will be created, If Source Folder contains CSV's then Create Table with the Name of CSV, If Contains Both then do both)
+        //Creates New Folder in AchiceFolder for Each Folder in SourceFolder, If CSV's not in Folder Create Folder
+        //When Done Loading Folder of CSV's, Or Single CSV Not in Folder, Move to ArchiveFolder and Zip the Table Folder - Assuming No Errors, If Errors in FFolder of Multiple CSV (e.g. 4 out of 5 Csv For Table1 Loaded) Move Files which loaded to Archive DO NOT ZIP - Error Log Display Folder And Files Or Just Files that Failed to Load 
+        //Add Check to see if Table Folder is empty if Yes then Zip Archive
+        //When Done Loading Files Add File Information to To Soulution DB for Tracking and Documentation Purposes
+        //Creates New Folder Called ArchiveFolder- Check if one exhists - Derive frome SolutionFolderPath
+        //Creates New Folder Called LogsFolder- Check if one exhits - 
 
+        //********************START HERE FOR CLEANING******************
+        // Get Table Columns
+        /*
+        string TableCols = "USE " + DBName + "'";
+        TableCols += "SELECT c.name ColumnName FROM sys.columns c";
+            TableCols += "INNER JOIN sys.tables t ON c.object_id = t.object_id INNER JOIN sys.schemas s ON t.schema_id = s.schema_id";
+            TableCols += "WHERE t.name = '" + TableName + "'";
+            //Should return List of columns given the DBName and TableName
+            //TODO Iter through Cols, 
+            //FOR Each Col in Collist
+            string ColMaxLen = "USE " + DBName + "'";
+        ColMaxLen += "SELECT";
+            ColMaxLen += "MAX(LEN([" + ColName + "]))";
+            ColMaxlen += "FROM [" + TableName + "]";
+
+            //For Each Row in Col 
+            Detect any nulls
+            Detect Data Type
+
+            //
+        string AlterCol = "ALTER TABLE [" + TableName + "]";
+        AlterCol += "ALTER COLUMN [" + ColName + "] NVARCHAR(" + ColMaxLen + ")";
+        
+            bool success = true;*/
         public static List<string> splitCSV(string line)
         {
             List<string> buff = new List<string>();
@@ -97,7 +130,7 @@ namespace ST_9fe3f0c506744f589075f4e22c70b50b
             for (int i = 0; i < line.Length; i++)
             {
                 char at = line[i];
-                if (! escape)
+                if (!escape)
                 {
                     if (at == '"') //Start of an escape sequence
                     {
@@ -118,7 +151,7 @@ namespace ST_9fe3f0c506744f589075f4e22c70b50b
                     bool end = (i + 1 == line.Length);
                     if (at == '"' && (end || line[i + 1] == ',')) //Encountered end of escape sequence, fall back to standard parsing
                     {
-                        escape = false; 
+                        escape = false;
                     }
                     else if (at == '"' && line[i + 1] == '"') //Encountered double ", skip the current quote 
                     {
@@ -152,11 +185,60 @@ namespace ST_9fe3f0c506744f589075f4e22c70b50b
             List<string> buff = new List<string>();
             foreach (string col in columns)
             {
-                buff.Add("'" + col.Replace("'", "''") + "'");
+                buff.Add("   '" + col.Replace("'", "''") + "'");
             }
 
-            return string.Join(",", buff);
+            return string.Join(",\n", buff);
         }
+
+        public static string columnHeaders(string line, char delimiter)
+        {
+            List<string> columns = (delimiter == ',' ? splitCSV(line) : splitDefault(line, delimiter));
+            List<string> buff = new List<string>();
+            foreach (string col in columns)
+            {
+                buff.Add("   " + col.Replace(" ", "_") + " nvarchar(max)");
+            }
+            buff.Add("   FileName nvarchar(max)");
+
+            return string.Join(",\n", buff);
+        }
+
+        public string createTable(string DBName, string fileName, string fileDelimiter, StreamWriter OUT)
+        {
+            using (SqlConnection myADONETConnection = (SqlConnection)(Dts.Connections["DB_Conn_StagingArea"].AcquireConnection(Dts.Transaction) as SqlConnection))
+            {
+                using (System.IO.StreamReader SourceFile = new System.IO.StreamReader(fileName))
+                {
+                    string line = SourceFile.ReadLine();
+                    if (line == null)
+                    {
+                        throw new Exception("Unable to read from: " + fileName);
+                    }
+
+                    string columnList = columnHeaders(line, fileDelimiter.ToCharArray()[0]);
+                    string tableName = Path.GetFileNameWithoutExtension(fileName).Replace(" ", "_").Replace("(", "").Replace(")", "").Replace("@", "").Replace("'", "");
+
+                    string createTable = "USE " + DBName + "\n" +
+                                         " IF EXISTS (SELECT * FROM sys.tables WHERE name = N'" + tableName + "')\n" +
+                                         "   DROP TABLE " + tableName + "\n" +
+                                         " CREATE TABLE " + tableName + " (\n" + columnList + "\n)";
+
+                    OUT.WriteLine(createTable);
+
+                    SqlCommand CreateTableCmd = new SqlCommand(createTable, myADONETConnection);
+                    CreateTableCmd.ExecuteNonQuery();
+
+                    return "[dbo].[" + tableName + ']';
+                }
+            }
+        }
+
+        private Dictionary<string, int> maxColumnLengths(string[] fileEntries)
+        {
+            throw new NotImplementedException();
+        }
+
 
         private static string logFolder = null;
 
@@ -175,10 +257,10 @@ namespace ST_9fe3f0c506744f589075f4e22c70b50b
             logFolder = Dts.Variables["User::LogFolder"].Value.ToString();
             string FileExtension = Dts.Variables["User::FileExtension"].Value.ToString();
             string FileDelimiter = Dts.Variables["User::FileDelimiter"].Value.ToString();
-            string TableName = Dts.Variables["User::DestinationTable"].Value.ToString();
+            string DBName = Dts.Variables["User::DBName"].Value.ToString();
             string ArchiveFolder = Dts.Variables["User::ArchiveFolder"].Value.ToString();
             //string ColumnList = "";
-
+            
             bool success = true;
 
             //Reading file names one by one
@@ -187,66 +269,81 @@ namespace ST_9fe3f0c506744f589075f4e22c70b50b
 
             using (StreamWriter OUT = new StreamWriter(Dts.Variables["User::LogFolder"].Value.ToString() + "\\" + "OutputLog_" + datetime + ".log", true))
             {
-                DateTime startAll = DateTime.Now;
-                Parallel.ForEach(fileEntries, (fileName, loopState) =>
+                try
                 {
-                    SqlConnection myADONETConnection = new SqlConnection();
-                    using (myADONETConnection = (SqlConnection)(Dts.Connections["DB_Conn_StagingArea"].AcquireConnection(Dts.Transaction) as SqlConnection))
+                    Dictionary<string, int> columnLengths = this.maxColumnLengths(fileEntries);
+                    string TableName = this.createTable(DBName, fileEntries[0], FileDelimiter, OUT);
+                    OUT.WriteLine("Importing " + fileEntries.Length + " source files into " + TableName);
+
+                    DateTime startAll = DateTime.Now;
+                    Parallel.ForEach(fileEntries, (fileName, loopState) =>
                     {
-                        try
+                        SqlConnection myADONETConnection = new SqlConnection();
+                        using (myADONETConnection = (SqlConnection)(Dts.Connections["DB_Conn_StagingArea"].AcquireConnection(Dts.Transaction) as SqlConnection))
                         {
+                            try
+                            {
                             //Writing Data of File Into Table
                             int counter = 0;
-                            string line;
+                                string line;
                             //MessageBox.Show(fileName);
 
                             DateTime start = DateTime.Now;
-                            using (System.IO.StreamReader SourceFile = new System.IO.StreamReader(fileName))
-                            {
-                                while ((line = SourceFile.ReadLine()) != null)
+                                using (System.IO.StreamReader SourceFile = new System.IO.StreamReader(fileName))
                                 {
+                                    while ((line = SourceFile.ReadLine()) != null)
+                                    {
                                     //TODO: HACK TO CLEAN OFF NORMALIZATION IN EXCEL REMOVE ME!!
-                                    line = line.Replace(":apos", "'").Replace(":comma", ",");
+                                    //line = line.Replace(":apos", "'").Replace(":comma", ",");
 
                                     //MessageBox.Show(line.ToString());
                                     if (counter > 0)
                                     {
-                                        string query = "Insert into " + TableName + " Values (" +
+                                        string query = "USE " + DBName + "\n" +
+                                                        " INSERT INTO " + TableName + " VALUES (\n" +
                                                         normalize(line, FileDelimiter.ToCharArray()[0]) +
-                                                        ",'" + fileName + "')";
+                                                        ",\n   '" + fileName + "'\n)";
 
-                                        //MessageBox.Show(query.ToString());
+                                        OUT.WriteLine("Running query for: " + fileName + "\n" + query);
                                         SqlCommand myCommand1 = new SqlCommand(query, myADONETConnection);
-                                        myCommand1.ExecuteNonQuery();
-                                    }
+                                            myCommand1.ExecuteNonQuery();
+                                        }
 
-                                    counter++;
+                                        counter++;
+                                    }
                                 }
-                            }
-                            OUT.WriteLine("Took " + (DateTime.Now - start).TotalSeconds + " seconds to execute " + counter + " import statements for " + fileName + ".");
+                                OUT.WriteLine("Took " + (DateTime.Now - start).TotalSeconds + " seconds to execute " + counter + " import statements for " + fileName + ".");
 
                             //move the file to archive folder after adding datetime to it
                             File.Move(fileName, ArchiveFolder + "\\" + (fileName.Replace(SourceFolderPath, "")).Replace(FileExtension, "") + "_" + datetime + FileExtension);
 
-                            OUT.WriteLine("Took " + (DateTime.Now - start).TotalSeconds + " seconds to import and archive " + fileName + ".");
-                        }
-                        catch (Exception exception)
-                        {
-                            loopState.Break();
-
-                            // Create Log File for Errors
-                            using (StreamWriter sw = new StreamWriter(Dts.Variables["User::LogFolder"].Value.ToString()
-                                + "\\" + "ErrorLog_" + datetime + ".log", true))
+                                OUT.WriteLine("Took " + (DateTime.Now - start).TotalSeconds + " seconds to import and archive " + fileName + ".");
+                            }
+                            catch (Exception exception)
                             {
-                                sw.WriteLine(exception.ToString());
-                                success = false;
+                                loopState.Break();
+
+                                // Create Log File for Errors
+                                using (StreamWriter sw = new StreamWriter(Dts.Variables["User::LogFolder"].Value.ToString() + "\\" + "ErrorLog_" + datetime + ".log", true))
+                                {
+                                    sw.WriteLine(exception.ToString());
+                                    success = false;
+                                }
                             }
                         }
-                    }
-                });//END Paralell add
+                    });//END Paralell add
 
-                OUT.WriteLine("Took " + (DateTime.Now - startAll).TotalSeconds + " seconds to import " + fileEntries.Length + " raw files.");
-                OUT.WriteLine(success ? "Success!!" : "Failure :C");
+                    OUT.WriteLine("Took " + (DateTime.Now - startAll).TotalSeconds + " seconds to import " + fileEntries.Length + " raw files.");
+                    OUT.WriteLine(success ? "Success!!" : "Failure :C");
+                } catch (Exception exception)
+                {
+                    // Create Log File for Errors
+                    using (StreamWriter sw = new StreamWriter(Dts.Variables["User::LogFolder"].Value.ToString() + "\\" + "ErrorLog_" + datetime + ".log", true))
+                    {
+                        sw.WriteLine(exception.ToString());
+                        success = false;
+                    }
+                }
             }
 
             if (success)
@@ -273,5 +370,5 @@ namespace ST_9fe3f0c506744f589075f4e22c70b50b
         };
         #endregion
 
-	}
+    }
 }
